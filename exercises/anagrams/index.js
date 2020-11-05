83d89d5dd677a48fb59afdebec403697969f7f49
Conrad <cnrdcasper@gmail.com>
2020-11-05 16:58:01 -0500

    solved

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const charMap1 = charMapBuilder(stringA);
+    const charMap2 = charMapBuilder(stringB);
+
+    if(Object.keys(charMap1).length !== Object.keys(charMap2).length){
+        return false
+    }
+
+    for (let char in charMap1) {
+        if (charMap1[char] !== charMap2[char]) {
+            return false;
+        }
+        return true;
+    }
+}
+
+const charMapBuilder = (string) => {
+    let charMap = {};
+    for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+      if(charMap[char]){
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    }
+    return charMap
+  }
 
 module.exports = anagrams;
+
+
+// word.replace(/[^\w]/g, "").toLowerCase();
